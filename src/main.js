a8081b140cd6ce774cea5b078584b2ee0b7e28ae
pramono <pramono6236@gmail.com>
2024-12-05 11:38:58 +0700

    chore: :alien: setup default axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router";
+import apiClient from './plugins/axios';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.provide("axios", apiClient)
+app.mount("#app")
+
+// createApp(App).use(router).mount('#app')
 
