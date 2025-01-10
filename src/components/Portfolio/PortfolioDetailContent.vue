64a7b1e559e03154d51f6b0ea5345a764ff499fd
pramono <pramono6236@gmail.com>
2025-01-10 18:39:37 +0700

    style: :lipstick: add margin to ordered list in portfolio description for improved layout in PortfolioDetailContent

--- src/components/Portfolio/PortfolioDetailContent.vue
+++ src/components/Portfolio/PortfolioDetailContent.vue
@@ -61,7 +61,7 @@ portfolio.value = await response.data;
         <div class="divider divider-error"></div>
     
         <div class="my-4 w-full">
-          <p class="text-black dark:text-white mb-4 prose prose-sm max-w-none prose-strong:text-black dark:prose-strong:text-white space-y-2" v-html="portfolio.description"></p>
+          <p class="text-black dark:text-white mb-4 prose prose-sm max-w-none prose-strong:text-black dark:prose-strong:text-white space-y-2 prose-ol:ml-5" v-html="portfolio.description"></p>
         </div>
 
         <!-- <div class="divider divider-error"></div> -->
