85dd94968cdbde65bfba275c307d8e3b2560a13c
pramono <pramono6236@gmail.com>
2025-01-15 19:35:29 +0700

    style: :lipstick: update PortfolioContent component layout and improve image alt text for better accessibility

--- src/components/Portfolio/PortfolioContent.vue
+++ src/components/Portfolio/PortfolioContent.vue
@@ -11,16 +11,15 @@ const portfolios = ref(null);
 
 const response = await axios.get("/portfolios");
 portfolios.value = await response.data;
-
 </script>
 
 <template>
   <ContentCard title="Portfolio">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div
+        class="card card-compact bg-transparent dark:bg-base-300 w-full shadow-xl"
         v-for="(item, index) in portfolios"
         :key="index"
-        class="bg-transparent mb-3 dark:border-2 overflow-hidden dark:border-neutral-600 rounded-md image-full w-full shadow-xl"
       >
         <figure>
           <img
@@ -29,18 +28,21 @@ portfolios.value = await response.data;
                 ? item.images[0]
                 : 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
             "
-            alt="project images"
+            alt="Preview Image"
           />
         </figure>
-        <div class="card-body bg-none">
-          <p class="text-muted font-semibold">{{ item.type }}</p>
+        <div class="card-body">
           <h2 class="card-title text-title">{{ item.name }}</h2>
+          <p class="text-title">{{ item.type }}</p>
           <div class="card-actions justify-end">
-            <router-link :to="'portfolio/' + item.id" class="btn btn-sm btn-primary">Learn More</router-link>
+            <router-link
+              :to="'portfolio/' + item.id"
+              class="btn btn-sm btn-primary"
+              >Learn More</router-link
+            >
           </div>
         </div>
       </div>
     </div>
   </ContentCard>
-
 </template>
