4a34070023a9ffa753850081f58e9d7d68ce2edb
pramono <pramono6236@gmail.com>
2025-03-20 01:25:09 +0700

    style: :lipstick: remove Knowledge and Contact components to streamline the application and improve maintainability

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createMemoryHistory, createRouter, createWebHistory } from "vue-router"
 
 import HomeVue from "../views/Home.vue";
 import AboutVue from "../views/About.vue";
-import ContactVue from "../views/Contact.vue";
 import PortfolioVue from "../views/Portfolio.vue";
 import ResumeVue from "../views/Resume.vue";
 import PageNotFound from "../views/errors/404.vue"
@@ -12,7 +11,6 @@ const routes = [
   { path: "/", name: "home", component: HomeVue },
   { path: "/about", name: "about", component: AboutVue },
   { path: "/resume", name: "resume", component: ResumeVue },
-  { path: "/contact", name: "contact", component: ContactVue },
   { path: "/portfolio", name: "portfolio", component: PortfolioVue },
   { path: "/portfolio/:id", name: "portfolio-detail", component: PortfolioDetailVue },
   { path: '/:pathMatch(.*)*', component: PageNotFound },
