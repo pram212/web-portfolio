dd5a954dd8e21c58d049a2bcee1e7eb8c75ba17f
pramono <pramono6236@gmail.com>
2025-03-20 01:28:53 +0700

    style: :lipstick: clean up LinkInfo component by removing unused code and simplifying data handling

--- src/components/Home/LinkInfo.vue
+++ src/components/Home/LinkInfo.vue
@@ -2,24 +2,6 @@
 import { onMounted, ref, inject } from "vue";
 import socialMedia from "../../resources/datas/links.json";
 
-// await new Promise(resolve => setTimeout(resolve, 500))
-
-// instance axios setup from main
-const axios = inject("axios")
-
-console.log(socialMedia);
-
-
-const links = ref(null);
-
-// if (import.meta.env.VITE_USE_SERVICE) {
-//   console.log("aktif");
-//   const fetchLink = await axios.get("/links");
-//   links.value = await fetchLink.data;
-// } else {
-//   links.value = socialMedia
-// }
-
 </script>
 
 <template>
@@ -32,7 +14,6 @@ const links = ref(null);
       class="bg-transparent dark:bg-neutral-800 p-[10px] rounded-md hover:btn-primary"
     >
       <img :src="item.svg" class="h-8 w-8" alt="">
-      <svg class="h-8 w-8" v-html="item.svg"></svg>
     </a>
   </div>
 </template>
