e83e029f4a1ec05b8d32be118cbea176f9a4c9f8
pramono <pramono6236@gmail.com>
2025-04-06 00:27:28 +0700

    style: :lipstick: add Supabase integration for portfolio data retrieval and update .gitignore and package.json

--- src/components/Portfolio/PortfolioContent.vue
+++ src/components/Portfolio/PortfolioContent.vue
@@ -1,35 +1,27 @@
 <script setup>
-import { formatDate } from "../../helpers";
-import { onMounted, ref, inject, computed } from "vue";
+import { ref } from "vue";
 import ContentCard from "../ContentCard.vue";
-import portfolios from "../../resources/datas/portfolios.json"
+import { supabase } from "../../lib/supabaseClient";
 
-const projects = computed(() => {
-  return portfolios.map(project => ({
-    ...project, // Menyalin semua properti project
-    images: JSON.parse(project.images) // Convert string ke array
-  }))
-})
+const projects = ref([])
+
+const { data } = await supabase.from('portfolios').select()
+projects.value = data.map(project => ({
+  ...project, // Menyalin semua properti project
+  images: JSON.parse(project.images) // Convert string ke array
+}))
 
 </script>
 
 <template>
   <ContentCard title="Portfolio">
     <div class="md:grid md:grid-cols-3 md:gap-4 space-y-3 md:space-y-0">
-      <div
-        class="card bg-transparent dark:bg-base-300 w-full shadow-xl"
-        v-for="(item, index) in projects"
-        :key="index"
-      >
+      <div class="card bg-transparent dark:bg-base-300 w-full shadow-xl" v-for="(item, index) in projects" :key="index">
         <figure class="h-36 overflow-hidden">
-          <img
-            :src="
-              Array.isArray(item.images)
-                ? item.images[0]
-                : 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
-            "
-            alt="Preview Image"
-          />
+          <img :src="Array.isArray(item.images)
+            ? item.images[0]
+            : 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
+            " alt="Preview Image" />
         </figure>
         <div class="card-body">
           <h2 class="card-title text-title">{{ item.project_title }}</h2>
@@ -37,11 +29,7 @@ const projects = computed(() => {
             {{ item.type }}
           </p>
           <div class="card-actions justify-end">
-            <router-link
-              :to="'portfolio/' + item.id"
-              class="btn btn-sm btn-primary"
-              >Learn More</router-link
-            >
+            <router-link :to="'portfolio/' + item.id" class="btn btn-sm btn-primary">Learn More</router-link>
           </div>
         </div>
       </div>
--- src/components/Portfolio/PortfolioDetailContent.vue
+++ src/components/Portfolio/PortfolioDetailContent.vue
@@ -25,51 +25,46 @@ console.log(portfolio.value)
 </script>
 
 <template>
-    <div>
-        <div class="flex items-center">
-          <h3 class="text-lg font-bold text-red-500 mx-auto">
-            {{ portfolio.project_title }}
-          </h3>
-        </div>
-        <div class="divider divider-error"></div>
-        <figure class="bg-neutral-700">
-          <Carousel
-            :autoplay="3000"
-            :items-to-show="1"
-            :wrap-around="true"
-            class="mb-10"
-          >
-            <Slide v-for="(item, index) in JSON.parse(portfolio.images)" :key="index">
-              <div class="carousel__item">
-                <img :src="item" alt="" class="h-96 mx-auto" />
-              </div>
-            </Slide>
-          </Carousel>
-        </figure>
-
-        <div class="mt-4">
-          <p class="capitalize text-title">
-              <span class="font-semibold">Type</span> :
-              <span>{{ portfolio.type }}</span>
-            </p>
-            <p class="capitalize text-title">
-              <span class="font-semibold">Date</span> :
-              <span>
-                  {{ formatDate(portfolio.start) }} - {{ formatDate(portfolio.end) }}</span
-              >
-            </p>
-            <p class="text-title" v-if="portfolio.demo">
-              <span class="font-semibold">Demo</span> :
-              <a target="_blank" :href="portfolio.demo?.url">{{
-                portfolio.demo?.url
-              }}</a>
-            </p>
-        </div>
-
-        <div class="divider divider-error"></div>
-    
-        <div class="my-4 w-full">
-          <p class="text-black dark:text-white mb-4 prose prose-sm max-w-none prose-strong:text-black dark:prose-strong:text-white space-y-2 prose-ol:ml-5" v-html="portfolio.description"></p>
-        </div>
+  <div>
+    <div class="flex items-center">
+      <h3 class="text-lg font-bold text-red-500 mx-auto">
+        {{ portfolio.project_title }}
+      </h3>
     </div>
+    <div class="divider divider-error"></div>
+    <figure class="bg-neutral-700">
+      <Carousel :autoplay="3000" :items-to-show="1" :wrap-around="true" class="mb-10">
+        <Slide v-for="(item, index) in JSON.parse(portfolio.images)" :key="index">
+          <div class="carousel__item">
+            <img :src="item" alt="" class="h-96 mx-auto" />
+          </div>
+        </Slide>
+      </Carousel>
+    </figure>
+
+    <div class="mt-4">
+      <p class="capitalize text-title">
+        <span class="font-semibold">Type</span> :
+        <span>{{ portfolio.type }}</span>
+      </p>
+      <p class="capitalize text-title">
+        <span class="font-semibold">Date</span> :
+        <span>
+          {{ formatDate(portfolio.start) }} - {{ formatDate(portfolio.end) }}</span>
+      </p>
+      <p class="text-title" v-if="portfolio.demo">
+        <span class="font-semibold">Demo</span> :
+        <a target="_blank" :href="portfolio.demo?.url">{{
+          portfolio.demo?.url
+        }}</a>
+      </p>
+    </div>
+
+    <div class="divider divider-error"></div>
+
+    <div class="my-4 w-full">
+      <p class="text-black dark:text-white mb-4 prose prose-sm max-w-none prose-strong:text-black dark:prose-strong:text-white space-y-2 prose-ol:ml-5"
+        v-html="portfolio.description"></p>
+    </div>
+  </div>
 </template>
