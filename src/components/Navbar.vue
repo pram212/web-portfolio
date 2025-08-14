6e9ffa5cc8e9777f8bef28ac8534ed6e422e594a
pramono <pramono6236@gmail.com>
2025-08-14 17:05:00 +0700

    fix bugs link aktif

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { defineProps, ref } from 'vue';
 import Link from './Link.vue';
+import { useRoute } from 'vue-router';
+const route = useRoute()
 
 const props = defineProps({
     showSidebar: Boolean,
@@ -10,12 +12,13 @@ const props = defineProps({
 </script>
 
 <template>
-    <nav class="mx-auto md:pt-6 w-full md:py-2 lg:px-10 bg-gradient-to-b from-white to-transparent from-20% dark:bg-gradient-to-b dark:from-neutral-800 dark:to-transparent">
+    <nav
+        class="mx-auto md:pt-6 w-full md:py-2 lg:px-10 bg-gradient-to-b from-white to-transparent from-20% dark:bg-gradient-to-b dark:from-neutral-800 dark:to-transparent">
         <div class="flex justify-between w-full">
             <h1 class="font-brand animate-fade-right pl-2">Pramono</h1>
             <ul class="menu menu-horizontal rounded-box space-x-5 hidden md:flex">
                 <li>
-                    <Link to="/">
+                    <Link to="/" :class="{'link-active' : route.name == 'home'}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -24,7 +27,7 @@ const props = defineProps({
                     Home</Link>
                 </li>
                 <li>
-                    <Link to="/about">
+                    <Link to="/about" :class="{'link-active' : route.name == 'about'}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -34,7 +37,7 @@ const props = defineProps({
                     </Link>
                 </li>
                 <li>
-                    <Link to="/resume">
+                    <Link to="/resume" :class="{'link-active' : route.name == 'resume'}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -44,7 +47,7 @@ const props = defineProps({
                     </Link>
                 </li>
                 <li>
-                    <Link to="/portfolio">
+                    <Link to="/portfolio" :class="{'link-active' : route.name?.startsWith('portfolio')}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -57,14 +60,14 @@ const props = defineProps({
                     <label class="swap swap-rotate bg-error rounded-full p-2">
                         <!-- this hidden checkbox controls the state -->
                         <input type="checkbox" class="theme-controller" @change="$emit('themeToggle')" />
-    
+
                         <!-- sun icon -->
                         <svg class="swap-on h-7 w-7 fill-current" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24">
                             <path
                                 d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
                         </svg>
-    
+
                         <!-- moon icon -->
                         <svg class="swap-off h-7 w-7 fill-current" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24">
@@ -79,14 +82,14 @@ const props = defineProps({
                     <label class="swap swap-rotate bg-error rounded-full p-2">
                         <!-- this hidden checkbox controls the state -->
                         <input type="checkbox" class="theme-controller" @change="$emit('themeToggle')" />
-    
+
                         <!-- sun icon -->
                         <svg class="swap-on h-7 w-7 fill-current" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24">
                             <path
                                 d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
                         </svg>
-    
+
                         <!-- moon icon -->
                         <svg class="swap-off h-7 w-7 fill-current" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24">
@@ -96,18 +99,19 @@ const props = defineProps({
                     </label>
                 </li>
                 <li>
-                    <label for="my-drawer" @click.prevent="$emit('sidebar-toggle')" class="btn btn-circle swap swap-rotate bg-error border-none p-2">
+                    <label for="my-drawer" @click.prevent="$emit('sidebar-toggle')"
+                        class="btn btn-circle swap swap-rotate bg-error border-none p-2">
                         <!-- this hidden checkbox controls the state -->
                         <!-- <input type="checkbox" class="theme-controller"/> -->
                         <!-- hamburger icon -->
-                        <svg class="h-7 w-7 fill-current" :class="{ 'swap-on': showSidebar, 'swap-off' : !showSidebar}" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512">
+                        <svg class="h-7 w-7 fill-current" :class="{ 'swap-on': showSidebar, 'swap-off': !showSidebar }"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
                         </svg>
 
                         <!-- close icon -->
-                        <svg class="h-7 w-7 fill-current" :class="{ 'swap-off': showSidebar, 'swap-on' : !showSidebar}" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512">
+                        <svg class="h-7 w-7 fill-current" :class="{ 'swap-off': showSidebar, 'swap-on': !showSidebar }"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <polygon
                                 points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
                         </svg>
@@ -117,3 +121,10 @@ const props = defineProps({
         </div>
     </nav>
 </template>
+
+<style>
+.link-active {
+    @apply bg-gradient-to-r from-[#FA5252] to-[#DD2476] font-medium text-white
+}
+
+</style>
