b8c039071a68242be18d78c12faa8ddea5b2497e
pramono <pramono6236@gmail.com>
2025-12-23 15:38:10 +0700

    refactor: remove skeleton components and update portfolio views
    - Deleted unused skeleton components: ClientSkeleton.vue, PortfolioDetailSkeleton.vue, and PortfolioSkeleton.vue.
    - Updated Portfolio.vue to use computed properties for project data and display project cards.
    - Enhanced PortfolioDetail.vue to include a carousel for project images and detailed project information.
    - Improved localization in en.json and id.json for project details and skills.

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,13 +1,35 @@
 <script setup>
-import PortfolioContent from "@components/Portfolio/PortfolioContent.vue"
-import PortfolioSkeletonVue from "@components/Skeletons/PortfolioSkeleton.vue"
+import { computed } from "vue"
+import ContentCard from "@components/ContentCard.vue"
+import { useI18n } from 'vue-i18n'
+
+const { tm } = useI18n()
+const projects = computed(() => tm('content.projects'))
+
 </script>
 
 <template>
-  <Suspense>
-    <PortfolioContent />
-    <template #fallback>
-      <PortfolioSkeletonVue />
-    </template>
-  </Suspense>
+  <ContentCard :title="$t('elements.work_page.header')">
+
+    <div class="md:grid md:grid-cols-3 md:gap-4 space-y-3 md:space-y-0">
+      <div class="card bg-transparent dark:bg-base-300 w-full shadow-xl" v-for="(item, index) in projects" :key="index">
+        <figure class="h-36 overflow-hidden">
+          <img :src="Array.isArray(item.images)
+            ? item.images[0]['src']
+            : 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
+            " alt="Preview Image" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title text-title">{{ item.title }}</h2>
+          <p class="text-title text-wrap">
+            {{ item.type }}
+          </p>
+          <div class="card-actions justify-end">
+            <router-link :to="'portfolio/' + index" class="btn btn-sm btn-primary">{{ $t('elements.buttons.read_more') }}</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </ContentCard>
 </template>
--- src/views/PortfolioDetail.vue
+++ src/views/PortfolioDetail.vue
@@ -1,16 +1,111 @@
 <script setup>
-import ContentCard from "@components/ContentCard.vue";
-import PortfolioDetailContent from "@components/Portfolio/PortfolioDetailContent.vue";
-import PortfolioDetailSkeleton from "@components/Skeletons/PortfolioDetailSkeleton.vue";
+import ContentCard from "@components/ContentCard.vue"
+import { Carousel, Slide } from "vue3-carousel"
+import { computed } from "vue"
+import "vue3-carousel/dist/carousel.css"
+import { useRoute } from "vue-router"
+import Link from "@components/Link.vue"
+import { useI18n } from 'vue-i18n'
+
+const route = useRoute()
+
+const { tm } = useI18n()
+const project = computed(() => tm('content.projects')[route.params.id])
+
 </script>
 
 <template>
   <ContentCard title="Portfolio Detail">
-    <Suspense>
-        <PortfolioDetailContent />
-        <template #fallback>
-            <PortfolioDetailSkeleton/>
-        </template>
-    </Suspense>
+    <div>
+      <div class="flex items-center justify-center mb-5">
+        <h3 class="text-2xl uppercase font-semibold text-red-500">
+          {{ project.title }}
+        </h3>
+      </div>
+      <div class="divider divider-error"></div>
+      <figure class="bg-transparent">
+        <Carousel :autoplay="3000" :items-to-show="1" :wrap-around="true"
+          class="mb-10 bg-gradient-to-b from-white to-purple-200  dark:bg-gradient-to-b dark:from-neutral-800 dark:to-neutral-900 rounded-lg shadow-lg">
+          <Slide v-for="(item, index) in project.images" :key="index">
+            <div class="carousel__item">
+              <img :src="item.src" alt="image portfolio" class="h-96" />
+            </div>
+          </Slide>
+        </Carousel>
+      </figure>
+
+      <div class="mt-4">
+        <table>
+          <tr>
+            <td class="capitalize font-semibold text-error">{{ $t('elements.work_page.work_detail.type') }} </td>
+            <td class="text-gray-700 dark:text-white">: {{ project.type }}</td>
+          </tr>
+          <tr>
+            <td class="capitalize font-semibold text-error">{{ $t('elements.work_page.work_detail.domain') }} </td>
+            <td class="text-gray-700 dark:text-white">: {{ project.domain }}</td>
+          </tr>
+          <tr>
+            <td class="capitalize font-semibold text-error">{{ $t('elements.work_page.work_detail.architecture') }} </td>
+            <td class="text-gray-700 dark:text-white">: {{ project.architecture }}</td>
+          </tr>
+          <tr>
+            <td class="capitalize font-semibold text-error">{{ $t('elements.work_page.work_detail.team') }} </td>
+            <td class="text-gray-700 dark:text-white">: {{ project.team }}</td>
+          </tr>
+          <tr>
+            <td class="capitalize font-semibold text-error">{{ $t('elements.work_page.work_detail.role') }} </td>
+            <td class="text-gray-700 dark:text-white">: {{ project.role }}</td>
+          </tr>
+          <tr v-if="project.demo">
+            <td class="capitalize font-semibold text-error">{{ $t('elements.work_page.work_detail.live_demo') }} </td>
+            <td>
+              <a target="_blank" :href="project.demo?.url">{{ project.demo?.url}}</a>
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <div class="divider divider-error"></div>
+      <!-- name of each tab group should be unique -->
+      <div role="tablist" class="tabs tabs-bordered">
+        <input type="radio" role="tab" name="my_tabs_3" class="tab text-error font-semibold" aria-label="Overview"
+          checked="checked" />
+        <div role="tabpanel" class="tab-content p-6">
+          <p v-for="value in project.description"
+            class="text-black dark:text-white mb-4 prose prose-sm max-w-none prose-strong:text-black dark:prose-strong:text-white space-y-2 prose-ol:ml-5">
+            {{ value }}
+          </p>
+        </div>
+
+        <input type="radio" role="tab" name="my_tabs_3" class="tab text-error font-semibold text-nowrap"
+          aria-label="Key Contributions" />
+        <div role="tabpanel" class="tab-content p-6">
+          <ul>
+            <li v-for="value in project.contributions"
+              class="text-black dark:text-white mb-4 prose prose-sm max-w-none prose-strong:text-black dark:prose-strong:text-white space-y-2 prose-ol:ml-5">
+              {{ value }}
+            </li>
+          </ul>
+        </div>
+
+        <input type="radio" role="tab" name="my_tabs_3" class="tab text-error font-semibold text-nowrap"
+          aria-label="Tech Stack" />
+        <div role="tabpanel" class="tab-content p-6">
+          <div class="flex flex-wrap gap-2">
+            <span v-for="(tech, index) in project.technologies.split(',')" :key="index"
+              class="btn btn-base-300 text-white font-semibold">
+              {{ tech }}
+            </span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="flex justify-end">
+        <Link to="/portfolio" class="mb-4 text-right">{{ $t('elements.buttons.back_to_portfolio') }}</Link>
+      </div>
+
+
+    </div>
   </ContentCard>
 </template>
