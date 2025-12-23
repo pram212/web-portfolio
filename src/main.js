547b5a5ab2f8e2fbfdd394a45c6edab7e98ceecc
pramono <pramono6236@gmail.com>
2025-12-23 16:05:13 +0700

    refactor: remove unused helper and API client files; update language files with new keys

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from "vue"
 import "@assets/style.css"
 import App from "./App.vue"
 import router from "./router"
-import apiClient from "./plugins/axios"
 import i18n from "./lang"
 
 
 const app = createApp(App)
 app.use(router)
 app.use(i18n)
-app.provide("axios", apiClient)
 app.mount("#app")
